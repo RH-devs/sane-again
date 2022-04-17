aa16afab2abca279d2e5d33e323dd600d32c972c
RH-devs <rasheedatajiborisha@gmail.com>
2022-04-17 18:12:49 +0000

    create heading for home page

--- saneagain/settings.py
+++ saneagain/settings.py
@@ -29,9 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['sane-again.herokuapp.com', 'localhost']
 
